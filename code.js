856366bd204e210c80749bf962f43d411440a7c4
Marek <marekkrupa03@gmail.com>
2022-07-05 21:02:18 +0200

    Added fnc returning index of book

--- code.js
+++ code.js
@@ -9,12 +9,16 @@ const $form = document.querySelector("form").addEventListener("submit", (e) => {
   clearForm();
 });
 
+const $list = document.querySelector("tbody");
+
 let myLibrary = [];
 
 const dataTest = [
   { title: "Mokebe", author: "Mokebe", pages: 123, status: "Read" },
   { title: "Mokebe", author: "Mokebe", pages: 123, status: "Not read" },
 ];
+
+showList();
 
 function Book(title, author, pages, status) {
   this.title = title;
@@ -31,6 +35,11 @@ function addBook() {
     $status.value
   );
   myLibrary.push(newBook);
+  //console.log(myLibrary.push(newBook))
+  updateStorage();
+}
+
+function updateStorage() {
   localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
 }
 
@@ -41,27 +50,29 @@ function checkLocalStorage() {
     myLibrary = dataTest;
   }
 }
-
-const $list = document.querySelector("tbody");
+//console.log(typeof myLibrary)
 
 function showList() {
   checkLocalStorage();
-  myLibrary.forEach((book) => {
+  $list.textContent = "";
+  myLibrary.forEach((book, index) => {
     const bookListed = `
       <tr>
         <td>${book.title}</td>
         <td>${book.author}</td>
         <td>${book.pages}</td>
         <td>
-        <button class="change">${book.status}</button>
+        <button class="change" data-value="${index}">${book.status}</button>
         </td>
         <td>
-        <button class="delete">Delete</button>
+        <button class="delete" data-value="${index}">Delete</button>
         </td>
-      </tr>
-      
-    `;
+      </tr> `;
     $list.insertAdjacentHTML("afterbegin", bookListed);
+  });
+
+  let deleteBtns = document.querySelectorAll(".delete").forEach((btn) => {
+    btn.addEventListener("click", (e) => findBook(e));
   });
 }
 
@@ -69,4 +80,9 @@ function clearForm() {
   $title.value = "";
   $author.value = "";
   $pages.value = "";
+}
+
+function findBook(index) {
+  console.log(index.target.getAttribute("data-value"));
+  return index.target.getAttribute("data-value");
 }
